374adfeac897133338197ffb5ab94b807a91cb30
SpaceCheetah <space-cheetah@hotmail.com>
2023-10-13 21:38:08 -0700

    Add search to R&D console

--- tgui/packages/tgui/interfaces/RDConsole.js
+++ tgui/packages/tgui/interfaces/RDConsole.js
@@ -1,6 +1,6 @@
 import { useBackend, useLocalState } from '../backend'
 import { Window } from '../layouts'
-import { Box, Flex, Button, Tooltip, Section} from '../components'
+import { Box, Flex, Button, Tooltip, Section, Input, Icon, Dropdown} from '../components'
 
 export const RDConsole = (props, context) => {
   const [page] = useLocalState(context, "page", Page.Home)
@@ -63,7 +63,7 @@ const TopBar = (props, context) => {
   const {data} = useBackend(context)
   const [_, setPage] = useLocalState(context, "page", Page.Home)
   return (
-    <Flex mb="1em" backgroundColor="rgba(0,70,0,0.4)" p="1em">
+    <Flex mb="1em" backgroundColor="rgba(0,0,0,0.25)" p="1em">
       <Flex.Item><Button icon="home" mr="1em" onClick={() => setPage(Page.Home)}/></Flex.Item>
       <Flex.Item><Button icon="cog" onClick={() => setPage(Page.Link)}/></Flex.Item>
       <Flex.Item textAlign="center" grow={1}>Last Action: {data.research_logs.length > 0 ? data.research_logs[data.research_logs.length - 1] : "None"}</Flex.Item>
@@ -72,13 +72,64 @@ const TopBar = (props, context) => {
   )
 }
 
+function arraySetOverlap(arr, set) {
+  for(const a in arr)
+    if(set.has(a))
+      return true
+  return false
+}
+
+function performSearch(context, search) {
+  const {data} = useBackend(context)
+  if(search.text === "")
+    return data.nodes
+  let filteredDesigns = new Set()
+  let filteredNodes = []
+  if(search.type == "designs")
+    for(const [key, value] of Object.entries(data.designs))
+      if(value.name.toLowerCase().indexOf(search.text.toLowerCase()) >= 0)
+        filteredDesigns.add(key)
+  for(const [key,value] of Object.entries(data.nodes))
+    if (((search.type === "name" && value.name.toLowerCase().indexOf(search.text.toLowerCase()) >= 0) ||
+        (search.type === "description" && value.description.toLowerCase().indexOf(search.text.toLowerCase()) >= 0) ||
+        (search.type === "designs" && arraySetOverlap(value.design_ids, filteredDesigns))))
+        filteredNodes.push(key)
+  return filteredNodes
+}
+
 const HomePage = (props, context) => {
-    const {researched, available, future} = getLists(context)
+  const {data} = useBackend(context)
+    let {researched, available, future} = getLists(context)
+    const [search, setSearch] = useLocalState(context, "search", {type: "name", text: ""})
+    const [searchResult, setSearchResult] = useLocalState(context, "searchResult", Object.keys(data.nodes))
+    const filter = (nodeID) => searchResult.indexOf(nodeID) !== -1
+    researched = researched.filter(filter)
+    available = available.filter(filter)
+    future = future.filter(filter)
     return (
       <Box>
         <Box width="25vw" bold inline textAlign="center">Researched</Box>
         <Box width="40vw" bold inline textAlign="center">Available</Box>
         <Box width="25vw" bold inline textAlign="center">Future</Box>
+        <Flex>
+          <Flex.Item>
+            <Icon name="search"/>
+          </Flex.Item>
+          <Flex.Item grow={1}>
+            <Input fluid onChange={(e, value) => {
+              search.text = value
+              setSearch(search)
+              setSearchResult(performSearch(context, search))
+            }} backgroundColor="rgba(0,0,0,0.3)"/>
+          </Flex.Item>
+          <Flex.Item>
+            <Dropdown onSelected={(value) => {
+              search.type = value
+              setSearch(search)
+              setSearchResult(performSearch(context, search))
+            }} options={["name", "description", "designs"]} selected="name" backgroundColor="rgba(0,0,0,0.3)"/>
+          </Flex.Item>
+        </Flex>
         <Box>
           {nodesToStack(researched, "25vw")}
           {nodesToStack(available, "40vw", true)}
